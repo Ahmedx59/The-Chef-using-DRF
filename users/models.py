905b2364e12b0124a646bdc17430cdae5bb2bc19
Ahmedx59 <Ahmedx59@gmail.com>
2024-10-29 21:30:09 +0300

    add models and create token from email user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+from django.utils.crypto import get_random_string
 
-# Create your models here.
+
+
+
+class User(AbstractUser):
+    class UserType(models.TextChoices):
+        SELLER = 'Seller'
+        USER = 'User'
+
+    email = models.EmailField(max_length=500 , unique=True)
+    name = models.CharField(max_length=100 )
+    image = models.ImageField(upload_to='user_image' , blank=True)
+    user_type = models.CharField(max_length=50 , choices=UserType.choices , default=UserType.USER)
+    code = models.CharField(max_length=50 , blank=True)
+    code_expire_date = models.DateTimeField(blank=True, null=True)
+
+    username = None
+    first_name = None
+    last_name = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["user_type","name"]
+
+    def __str__(self)-> str:
+        return self.email
+    
+
+    def save(self, *args, **kwargs):
+        if not self.code:
+            self.code = get_random_string(20)
+       
+        super().save(*args, **kwargs)
