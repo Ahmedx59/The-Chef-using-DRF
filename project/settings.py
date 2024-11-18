b4d126d33d5eecd570234bd89fa7c5d8f6446cd5
Ahmedx59 <Ahmedx59@gmail.com>
2024-11-18 22:03:28 +0200

    update and fix jwt

--- project/settings.py
+++ project/settings.py
@@ -147,13 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS= [
-    BASE_DIR / 'static'
-]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -166,4 +160,5 @@ from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=375),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=375),
+
 }
