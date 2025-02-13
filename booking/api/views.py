4ef6896cdfc41a6bd51a459b2a01adf20206b761
Ahmedx59 <Ahmedx59@gmail.com>
2025-02-13 22:25:38 +0200

    add models , class Booking in view  and ListBookingSerializer in serializers

--- booking/api/views.py
+++ booking/api/views.py
@@ -1,3 +1,17 @@
+from rest_framework import mixins , viewsets 
+
 from django.shortcuts import render
 
-# Create your views here.
+from booking.models import Booking
+from .serializers import (
+    ListBookingSerializer,
+                          
+)
+
+class booking(mixins.ListModelMixin,
+    viewsets.GenericViewSet
+    ):
+    queryset = Booking.objects.all()
+    serializer_class = ListBookingSerializer
+
+    
--- booking/models.py
+++ booking/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+
+from users.models import User
+from restaurant.models import Table 
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE , related_name='booking')
+    table = models.ManyToManyField(Table , related_name='booking')
+    code = models.CharField(max_length=50)
+    booking_date = models.DateTimeField() 
