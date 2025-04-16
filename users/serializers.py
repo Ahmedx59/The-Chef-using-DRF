b34f24049936b351715d2a8201cc4489d1bd027d
Ahmedx59 <Ahmedx59@gmail.com>
2025-04-16 21:45:20 +0200

    add sign up serializer and activate serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,15 @@
-from django.contrib.auth.hashers import make_password , check_password
 from rest_framework import serializers
+
+from django.contrib.auth.hashers import make_password , check_password
+from django.utils.crypto import get_random_string
+from django.core.validators import MinLengthValidator
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+
+from random import randint
+
 from .models import User
 
 
@@ -12,6 +22,65 @@ class UserSerializer(serializers.ModelSerializer):
             'image',
             'date_joined',
         ]
+class SignUPSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required = True)
+    email = serializers.CharField(required = True)
+    password = serializers.CharField(write_only = True , validators=[MinLengthValidator(8)] , required = True)
+    confirm_password = serializers.CharField(required = True , write_only = True)
+    # user_type = serializers.ChoiceField(choices=User.UserType.choices , required = True)
+    class Meta:
+        model = User
+        fields = ('email','username','password','confirm_password','user_type')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({"message":"password do not exist"})
+        
+        email_user = User.objects.filter(email = attrs['email']).first()
+        
+        if email_user:
+            raise serializers.ValidationError({"message":"email is exist"})
+        
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        validated_data["is_active"] = False
+        validated_data['code'] = randint(1000,9999)
+        user = User.objects.create_user(**validated_data)
+
+        send_mail(
+            f"Activation Code ",
+            f"welcome {validated_data['username']}\n Here is the activation code : {validated_data['code']}.",
+            settings.EMAIL_HOST_USER,
+            {validated_data['email']},
+            fail_silently=False,
+        )
+        return {}
+    
+    def to_representation(self, instance):
+        return {'message': 'chick your gmail.'}
+
+class ActivateSerializer(serializers.Serializer):
+    code = serializers.CharField(required = True)
+
+    def create(self, validated_data):
+        user_id = self.context['view'].kwargs['pk']
+        user = User.objects.get(id = user_id)
+
+        if user.code != validated_data['code']:
+            raise serializers.ValidationError({'message':'code is not correct'})
+        
+        user.is_active = True
+        user.code = ''
+        user.save()
+        
+        return {}
+
+
+
+
+
 
 
 class ChangePasswordSerializer(serializers.Serializer):
--- users/views.py
+++ users/views.py
@@ -2,18 +2,28 @@ from rest_framework.decorators import action
 from rest_framework import mixins , viewsets , status
 from rest_framework.response import Response 
 from .models import User
-from .serializers import UserSerializer , ChangePasswordSerializer
+from .serializers import (
+    UserSerializer ,
+    ChangePasswordSerializer ,
+    SignUPSerializer,
+    ActivateSerializer
+    )
 
 
 
 class UserViewSet(
     mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
     viewsets.GenericViewSet
         ):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return SignUPSerializer
+        return super().get_serializer_class()
 
     @action(detail=False , methods=['GET'])
     def profile(self , request):
@@ -21,7 +31,16 @@ class UserViewSet(
         # user = User.objects.get(id = 1)
         serializer = UserSerializer(user)
         return Response(serializer.data , status=status.HTTP_200_OK)
+    
 
+    @action(detail=True , methods=['post'] , serializer_class = ActivateSerializer )
+    def activate(self,request,*args, **kwargs):
+        data  = request.data
+        serializer = self.get_serializer(data = data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'message':'Your Account Activated Successfully'})
+    
 class AuthViewSet(viewsets.GenericViewSet):
     
     serializer_class = ChangePasswordSerializer
